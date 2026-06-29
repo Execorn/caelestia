961718f8827d962d004b43c705797be111deb9f2
Biryukov Nikita <n.biryukov.04@gmail.com>
2026-06-30 02:57:32 +0300

    feat(newsfeed): add newsfeed panel keybinding,Quotes database, and shell greeting alignment

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,10 +1,14 @@
 function fish_greeting
     echo -ne '\x1b[38;5;16m'  # Set colour to primary
-    echo '     ______           __          __  _       '
-    echo '    / ____/___ ____  / /__  _____/ /_(_)___ _ '
-    echo '   / /   / __ `/ _ \/ / _ \/ ___/ __/ / __ `/ '
-    echo '  / /___/ /_/ /  __/ /  __(__  ) /_/ / /_/ /  '
-    echo '  \____/\__,_/\___/_/\___/____/\__/_/\__,_/   '
+    echo '      ___  _  _____  _________  _________ '
+    echo '     / _ \| |/_/ _ \/ ___/ __ \/ ___/ __ \\'
+    echo '    /  __/>  </  __/ /__/ /_/ / /  / / / /'
+    echo '    \___/_/|_|\___/\___/\____/_/  /_/ /_/ '
     set_color normal
     fastfetch --key-padding-left 5
+
+    # Show random motivational quote
+    if test -f ~/.config/fastfetch/quotes.txt; and command -sq shuf
+        printf '    \e[3m\e[38;5;16m"%s"\e[0m\n\n' (shuf -n 1 ~/.config/fastfetch/quotes.txt)
+    end
 end
